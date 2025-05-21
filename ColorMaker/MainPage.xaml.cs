83810c889e8443efc401fac09c1b696fd97dc3b5
Wolvye <alainsoltau@googlemail.com>
2025-05-21 17:21:00 +0200

    Update

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace ColorMaker
+﻿
+namespace ColorMaker
 {
     public partial class MainPage : ContentPage
     {
@@ -9,6 +10,28 @@
             InitializeComponent();
         }
 
-       
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var red = (int)sldRed.Value;
+            var green = (int)sldGreen.Value;
+            var blue = (int)sldBlue.Value ;
+
+            lblRed.Text = red.ToString();
+            lblGreen.Text = green.ToString();
+            lblBlue.Text = blue.ToString();
+
+            Color color = Color.FromRgb(red, green, blue);
+
+            SetColor(color);
+            
+        }
+
+        private void SetColor(Color color)
+        {
+            btnRandom.BackgroundColor = color;
+            Container.BackgroundColor = color;
+            lblHex.Text = color.ToHex();
+            
+        }
     }
 }
