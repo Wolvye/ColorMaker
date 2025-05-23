2d5ceefa1d61e258b069b794fc90ed1c4b38f823
Wolvye <alainsoltau@googlemail.com>
2025-05-23 10:25:47 +0200

    Update

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -5,27 +5,32 @@ namespace ColorMaker
 {
     public partial class MainPage : ContentPage
     {
-      
 
+        bool isRandom; //Einfach damit im Output kein Dauerfeuer ist
         public MainPage()
         {
             InitializeComponent();
+           
         }
+
 
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            var red = (int)sldRed.Value;
-            var green = (int)sldGreen.Value;
-            var blue = (int)sldBlue.Value ;
+            if (!isRandom)
+            {
+                var red = (int)sldRed.Value;
+                var green = (int)sldGreen.Value;
+                var blue = (int)sldBlue.Value;
 
-            lblRed.Text = red.ToString();
-            lblGreen.Text = green.ToString();
-            lblBlue.Text = blue.ToString();
+                lblRed.Text = red.ToString();
+                lblGreen.Text = green.ToString();
+                lblBlue.Text = blue.ToString();
 
-            Color color = Color.FromRgb(red, green, blue);
+                Color color = Color.FromRgb(red, green, blue);
 
-            SetColor(color);
-            
+                SetColor(color);
+            }
+
         }
 
         private void SetColor(Color color)
@@ -33,20 +38,23 @@ namespace ColorMaker
             Debug.WriteLine(color.ToString());
             Container.BackgroundColor = color;
             lblHex.Text = color.ToHex();
+
             
         }
 
         private void btnRandom_Clicked(object sender, EventArgs e)
         {
+            isRandom = true;
             var random = new Random();
-            
-            var color = Color.FromRgb(random.Next(0,256),random.Next(0,256),random.Next(0,256));
+
+            var color = Color.FromRgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
 
             SetColor(color);
 
-            sldRed.Value = color.Red;
-            sldGreen.Value = color.Green;
-            sldBlue.Value = color.Blue;
+            sldRed.Value = color.Red * 255;
+            sldGreen.Value = color.Green * 255;
+            sldBlue.Value = color.Blue * 255;
+            isRandom = false;
         }
     }
 }
